baf0963e5cd9bc3715de66ad4a9ceec72896b699
aidasberesinevicius <aidas.beresinevicius@1clickfactory.com>
2019-12-05 10:32:54 +0200

    Step4 GiftForCustomer

--- ControlAddin/ControlAddinDemo.al
+++ ControlAddin/ControlAddinDemo.al
@@ -1,6 +1,8 @@
 controladdin ControlAddinDemo
 {
+    Scripts = 'scripts/jquery-3.3.1.min.js';
     StartupScript = 'scripts/start.js';
+    Images = 'img/present.png';
     StyleSheets = 'css/styles.css';
     RequestedHeight = 300;
     RequestedWidth = 300;
--- Scripts/start.js
+++ Scripts/start.js
@@ -1,1 +1,5 @@
+$('#controlAddIn').append('<div id="presents"></div>')
+var imgpath = Microsoft.Dynamics.NAV.GetImageResource("img/present.png");
+$('#presents').append('<img src="'+ imgpath + '" alt="present" width="100px" height="100px">');
+
 Microsoft.Dynamics.NAV.InvokeExtensibilityMethod("ControlAddinLoaded", []);
